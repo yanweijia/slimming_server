869ea5de62aafdef39de688c71588255a47bb2dd
yanweijia <happyboyywj@163.com>
2017-09-18 19:50:27 +0800

    user相关: changePassword.action 判断密码相同

--- src/main/java/cn/yanweijia/slimming/controller/GuestController.java
+++ src/main/java/cn/yanweijia/slimming/controller/GuestController.java
@@ -16,8 +16,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @author:  weijia
- * @func;    用户操作相关
+ * @author: weijia
+ * @func; 用户操作相关
  */
 @Controller
 @RequestMapping("/api/guest")
@@ -27,33 +27,38 @@ public class GuestController {
 
     /**
      * 登录接口
+     *
      * @param httpSession session
-     * @param username 用户名
-     * @param password 密码
+     * @param username    用户名
+     * @param password    密码
      * @return {"success":boolean,"message":string}
      */
-    @RequestMapping(value="/login.action",method= RequestMethod.POST)
-    public ResponseEntity<Map> login(HttpSession httpSession,@RequestParam String username, @RequestParam String password){
-        Map<String,Object> map = new HashMap<>();
-        int loginStatus = userService.login(httpSession,username,password);
+    @RequestMapping(value = "/login.action", method = RequestMethod.POST)
+    public ResponseEntity<Map> login(HttpSession httpSession, @RequestParam String username, @RequestParam String password) {
+        Map<String, Object> map = new HashMap<>();
+        int loginStatus = userService.login(httpSession, username, password);
 
 
-        map.put("success",loginStatus == UserServiceImpl.LOGIN_SUCCESS);
+        map.put("success", loginStatus == UserServiceImpl.LOGIN_SUCCESS);
         String msg;
-        switch(loginStatus){
+        switch (loginStatus) {
             case UserServiceImpl.LOGIN_SUCCESS:
-                msg = "登录成功";break;
+                msg = "登录成功";
+                break;
             case UserServiceImpl.LOGIN_USER_FORBIDDEN:
-                msg = "用户已被禁止登录";break;
+                msg = "用户已被禁止登录";
+                break;
             case UserServiceImpl.LOGIN_PW_NOT_MATCH:
-                msg = "用户名密码不匹配";break;
+                msg = "用户名密码不匹配";
+                break;
             case UserServiceImpl.LOGIN_INPUT_ILLEGAL:
-                msg = "请提供完整的账号密码.";break;
-            default:{
+                msg = "请提供完整的账号密码.";
+                break;
+            default: {
                 msg = "未知异常,无法登录,请联系管理员!";
             }
         }
-        map.put("message",msg);
+        map.put("message", msg);
         return new ResponseEntity<>(map, HttpStatus.OK);
     }
     //TODO:注册接口
--- src/main/java/cn/yanweijia/slimming/controller/UserController.java
+++ src/main/java/cn/yanweijia/slimming/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
@@ -17,8 +18,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @author:  weijia
- * @func;    用户操作相关
+ * @author: weijia
+ * @func; 用户操作相关
  */
 @Controller
 @RequestMapping("/api/user")
@@ -43,72 +44,80 @@ public class UserController {
 
     /**
      * 更新用户信息,验证密码并更新,仅限 POST<br/>
-     * 记得请求的时候 contentType设置为:<strong style='color:green'>application/json;charset=UTF-8</strong>
+     * 记得请求的时候 contentType设置为:
+     * <strong style='color:green'>
+     * application/json;charset=UTF-8
+     * </strong>
+     *
      * @param user User 的 JSON 格式请求
      * @return {"success":boolean,"message":string}
      */
-    @RequestMapping(value = "/updateUserInfo.action",method = RequestMethod.POST)
-    public ResponseEntity<Map> updateUserInfo(@RequestBody User user) throws IOException{
+    @RequestMapping(value = "/updateUserInfo.action", method = RequestMethod.POST)
+    public ResponseEntity<Map> updateUserInfo(@RequestBody User user) throws IOException {
         Map<String, Object> map = new HashMap<>();
         boolean success = userService.updateUserById(user) != 0;
-        map.put("success",success);
-        map.put("message",(success?"修改成功.":"修改失败."));
+        map.put("success", success);
+        map.put("message", (success ? "修改成功." : "修改失败."));
         return new ResponseEntity<>(map, HttpStatus.OK);
     }
 
 
     /**
      * 修改用户密码
-     * @param id 用户编号
+     *
+     * @param id    用户编号
      * @param oldPw 旧密码
      * @param newPw 新密码
      * @return {"success":boolean,"message":string}
      * @throws IOException
      */
-    @RequestMapping(value="/changePassword.action",method=RequestMethod.POST)
-    public ResponseEntity<Map> changePassword(@RequestParam Integer id,@RequestParam String oldPw,@RequestParam String newPw)throws  IOException{
-        Map<String,Object> map = new HashMap<>();
+    @RequestMapping(value = "/changePassword.action", method = RequestMethod.POST)
+    public ResponseEntity<Map> changePassword(@RequestParam Integer id, @RequestParam String oldPw, @RequestParam String newPw) throws IOException {
+        Map<String, Object> map = new HashMap<>();
         boolean success = false;
         String message = "未知错误";
-        if(id==null||id<0|| StringUtils.isEmpty(oldPw)||StringUtils.isEmpty(newPw)){
+        if (id == null || id < 0 || StringUtils.isEmpty(oldPw) || StringUtils.isEmpty(newPw)) {
             message = "参数传递有误";
-        }else{
+        } else if (oldPw.equals(newPw)) {
+            message = "旧密码与新密码相同,修改失败!";
+        } else {
             User user = userService.getUserById(id);
-            if(user!=null && user.getPassword().equals(oldPw)){
+            if (user != null && user.getPassword().equals(oldPw)) {
                 user = new User();
                 user.setId(id);
                 user.setPassword(newPw);
-                if(userService.updateUserByIdSelective(user)!=0) {
+                if (userService.updateUserByIdSelective(user) != 0) {
                     success = true;
                     message = "修改成功!";
-                }else
+                } else
                     message = "系统异常,修改失败";
-            }else {
+            } else {
                 message = "用户不存在或旧密码错误!";
             }
         }
-        map.put("success",success);
-        map.put("message",message);
+        map.put("success", success);
+        map.put("message", message);
         return new ResponseEntity<>(map, HttpStatus.OK);
     }
 
 
     /**
      * 登出(注销)
+     *
      * @param session session
      * @return {"success":boolean,"message":string}
      */
-    @RequestMapping(value="/logout.action")
-    public ResponseEntity<Map> logout(HttpSession session){
-        Map<String,Object> map = new HashMap<>();
+    @RequestMapping(value = "/logout.action")
+    public ResponseEntity<Map> logout(HttpSession session) {
+        Map<String, Object> map = new HashMap<>();
         boolean success = false;
-        if(session.getAttribute("id")!=null) {
+        if (session.getAttribute("id") != null) {
             session.removeAttribute("id");
             session.removeAttribute("username");
             success = true;
         }
-        map.put("success",success);
-        map.put("message",(success?"注销成功!":"注销失败,您本来就不在线上!"));
-        return new ResponseEntity<>(map,HttpStatus.OK);
+        map.put("success", success);
+        map.put("message", (success ? "注销成功!" : "注销失败,您本来就不在线上!"));
+        return new ResponseEntity<>(map, HttpStatus.OK);
     }
 }
